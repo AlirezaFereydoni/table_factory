dee70ee58aa88fe1eba815ff2296de38e976c032
Alireza Fereydoni <Aliirzw@gmail.com>
2024-06-06 19:42:42 +0330

    feat: add product app

---
+++ product/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Item, MDF, Section, Product
+# Register your models here.
+
+
+admin.site.register(Item)
+admin.site.register(MDF)
+admin.site.register(Section)
+admin.site.register(Product)
