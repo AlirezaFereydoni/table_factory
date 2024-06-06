d42be4880e5da25834e6cc1f1835d8d33bec0e23
Alireza Fereydoni <Aliirzw@gmail.com>
2024-06-06 19:43:08 +0330

    feat: add Item Class

---
+++ product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    size = models.CharField(max_length=100)
+    length = models.IntegerField()
+    count = models.IntegerField()
+    image = models.ImageField( null=True, blank=True )
+    description = models.TextField(max_length=300, default='', blank=True)
+    
+    
