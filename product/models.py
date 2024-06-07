3c1d5bb911d76cd76ba118e5c69d0e43f0935009
Alireza Fereydoni <Aliirzw@gmail.com>
2024-06-07 21:46:18 +0330

    chore: add created_at and updated_at

--- product/models.py
+++ product/models.py
@@ -14,12 +14,13 @@ class Item(models.Model):
         return f'{self.name} | {self.size} | {self.length} | {self.count}'
     
     
-    
 class MDF(models.Model):
     name = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     count = models.IntegerField(min=1)
     description = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.name} | {self.size} | {self.count}'
@@ -29,6 +30,8 @@ class Section(models.Model):
     items = models.ManyToManyField(Item,on_delete= models.CASCADE)
     MDFs = models.ManyToManyField(MDF, on_delete= models.CASCADE)
     image = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
@@ -38,6 +41,8 @@ class Product(models.Model):
     description = models.TextField(max_length=300, null=True, blank=True)
     vision = models.ImageField()
     series = models.ManyToManyField(Section, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.name} | {self.description}'
