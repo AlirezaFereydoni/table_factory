62ffaf3a108072dcfff23b57dc9745075c590de4
Alireza Fereydoni <Aliirzw@gmail.com>
2024-06-08 00:49:57 +0330

    chore: edit models and add timeStamp

--- product/models.py
+++ product/models.py
@@ -1,50 +1,53 @@
-from django.db import models
+from django.db.models import Model,DateTimeField,CharField,TextField,IntegerField,ImageField,ManyToManyField,CASCADE
 
 # Create your models here.
 
-class Item(models.Model):
-    name = models.CharField(max_length=100, blank=True, default=None)
-    size = models.CharField(max_length=100)
-    length = models.IntegerField(min=1, max=2000)
-    count = models.IntegerField(min=1)
-    image = models.ImageField(null=True, blank=True )
-    description = models.TextField(max_length=300, default=None, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)    
+class TimeStamp(Model): 
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)   
+    
+    class Meta:
+        abstract = True
+
+
+class Item(Model):
+    name = CharField(max_length=100, blank=True, default=None)
+    size = CharField(max_length=100)
+    length = IntegerField()
+    count = IntegerField()
+    image = ImageField(null=True, blank=True )
+    description = TextField(max_length=300, default=None, blank=True)
+    
     
     def __str__(self):
         return f'{self.name} | {self.size} | {self.length} | {self.count}'
     
     
-class MDF(models.Model):
-    name = models.CharField(max_length=100)
-    size = models.CharField(max_length=50)
-    count = models.IntegerField(min=1)
-    description = models.TextField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class MDF(Model):
+    name = CharField(max_length=100)
+    size = CharField(max_length=50)
+    count = IntegerField()
+    description = TextField(max_length=300)
+ 
     
     def __str__(self):
         return f'{self.name} | {self.size} | {self.count}'
     
-class Section(models.Model): 
-    name = models.CharField(max_length=100, blank=True, default=None)
-    items = models.ManyToManyField(Item,on_delete= models.CASCADE)
-    MDFs = models.ManyToManyField(MDF, on_delete= models.CASCADE)
-    image = models.ImageField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Section(Model): 
+    name = CharField(max_length=100, blank=True, default=None)
+    items = ManyToManyField(Item)
+    mdf_items = ManyToManyField(MDF)
+    image = ImageField(null=True)
+   
     
     def __str__(self):
         return self.name
     
-class Product(models.Model): 
-    name = models.CharField(max_length=100, null=False, blank=False)
-    description = models.TextField(max_length=300, null=True, blank=True)
-    vision = models.ImageField()
-    series = models.ManyToManyField(Section, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Product(Model): 
+    name = CharField(max_length=100, null=False, blank=False)
+    description = TextField(max_length=300, null=True, blank=True)
+    vision = ImageField()
+    series = ManyToManyField(Section)
     
     def __str__(self):
         return f'{self.name} | {self.description}'
